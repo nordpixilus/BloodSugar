13f660ff557a8ea073bce9f756197952e6db1842
Nikola <keha39@yandex.ru>
2023-08-21 20:42:00 +0300

    оптимизация создания таблицы

--- BloodSugar/Documents/SugarDocument.cs
+++ BloodSugar/Documents/SugarDocument.cs
@@ -1,163 +1,123 @@
 ﻿using BloodSugar.Models;
 using NetDayHospital.Core.Controls.ListBloodSugar;
 using NetDayHospital.Core.Documents;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace BloodSugar.Documents
+namespace BloodSugar.Documents;
+
+public class SugarDocument : BaseDocument
 {
-    public class SugarDocument : BaseDocument
+    private readonly Person person;
+    private readonly Table table = new();
+
+    public SugarDocument(Person person)
     {
-        private Person person;
+        this.person = person;                       
+    }
 
-        public SugarDocument(Person person)
+    public void Create()
+    {
+        AddText();            
+        AddTable();            
+    }       
+
+    private void AddText()
+    {
+        Paragraph paragraphPerson = new()
         {
-            this.person = person;
-            AddText();
-            AddTable();            
+            TextAlignment = TextAlignment.Center,
+            FontWeight = FontWeights.Bold
+        };
+
+        paragraphPerson.Inlines.Add(new LineBreak());
+        paragraphPerson.Inlines.Add(new LineBreak());
+        paragraphPerson.Inlines.Add(new Run($"{person.FullName}, дата рождения: {person.BirthDateFull}"));
+        paragraphPerson.Inlines.Add(new LineBreak());
+        paragraphPerson.Inlines.Add(new LineBreak());
+
+        Blocks.Add(paragraphPerson);
+
+        Blocks.Add(new Paragraph());
+
+        Paragraph paragraphTableName = new();
+        paragraphTableName.Inlines.Add(new Run(@"Уровень сахара крови  (ммоль\л)"));
+        paragraphTableName.Inlines.Add(new LineBreak());
+        paragraphTableName.TextAlignment = TextAlignment.Center;
+        paragraphTableName.FontWeight = FontWeights.Bold;
+        Blocks.Add(paragraphTableName);
+    }
+
+    private void AddTable()
+    {
+        table.FontSize = 19;
+        table.FontFamily = new FontFamily("Times New Roman");
+        table.CellSpacing = 2;
+        table.Padding = new Thickness(1);
+        table.Background = Brushes.Black;
+        table.Margin = new Thickness(150, 0, 150, 0);
+
+        table.Columns.Add(new TableColumn());
+        table.Columns.Add(new TableColumn());
+        table.Columns.Add(new TableColumn());
+
+        AddTableHeader();
+        AddTableContent();
+    }
+
+    private void AddTableHeader()
+    {
+        TableRow rowHeader = new();
+
+        rowHeader.Cells.Add(AddCellHeader("Дата"));
+        rowHeader.Cells.Add(AddCellHeader("Время"));
+        rowHeader.Cells.Add(AddCellHeader(@"Уровень сахара"));
+
+        TableRowGroup RowGroupHeader = new();
+        RowGroupHeader.Rows.Add(rowHeader);
+        table.RowGroups.Add(RowGroupHeader);
+    }
+
+    private void AddTableContent()
+    {
+        TableRowGroup RowGroupContent = new();
+        foreach (Sugar item in person.Sugars)
+        {
+            TableRow rowitem = new();
+            rowitem.Cells.Add(AddCellContent(item.DateOnly));
+            rowitem.Cells.Add(AddCellContent(item.TimeOnly));
+            rowitem.Cells.Add(AddCellContent(item.Level));
+            RowGroupContent.Rows.Add(rowitem);
         }
 
-        private void AddText()
+        table.RowGroups.Add(RowGroupContent);
+
+        Blocks.Add(table);
+    }
+
+    private static TableCell AddCellContent(string text)
+    {
+        Thickness thickness = new(0, 10, 0, 10);
+        return AddCell(text, thickness);
+    }
+
+    private static TableCell AddCellHeader(string text)
+    {
+        Thickness thickness = new(0, 5, 0, 15);
+        return AddCell(text, thickness);
+    }
+
+    private static TableCell AddCell(string text, Thickness thickness)
+    {
+        return new TableCell(new Paragraph(new Run(text)))
         {
-            Paragraph paragraphPerson = new();
-            paragraphPerson.TextAlignment = TextAlignment.Center;
-            paragraphPerson.FontWeight = FontWeights.Bold;
+            Background = Brushes.White,
+            TextAlignment = TextAlignment.Center,
+            Padding = thickness,
+            FontWeight = FontWeights.Bold,
 
-            paragraphPerson.Inlines.Add(new LineBreak());
-            paragraphPerson.Inlines.Add(new Run($"{person.FullName}, дата рождения: {person.BirthDateFull}"));
-            paragraphPerson.Inlines.Add(new LineBreak());
-            paragraphPerson.Inlines.Add(new LineBreak());
-
-            Blocks.Add(paragraphPerson);
-
-            Blocks.Add(new Paragraph());
-
-            Paragraph paragraphTableName = new();
-            paragraphTableName.Inlines.Add(new Run(@"Уровень сахара крови."));
-            paragraphTableName.Inlines.Add(new LineBreak());
-            paragraphTableName.TextAlignment = TextAlignment.Center;
-            paragraphTableName.FontWeight = FontWeights.Bold;
-            Blocks.Add(paragraphTableName);
-        }
-
-        private void AddTable()
-        {
-            Table table = new()
-            {
-                FontSize = 19,
-                FontFamily = new FontFamily("Times New Roman"),
-                Background = Brushes.Black, CellSpacing = 2,
-            };
-
-            TableColumn column1 = new();
-            TableColumn column2 = new();
-
-            table.Columns.Add(column1);
-            table.Columns.Add(column2);
-
-            TableRow row = new();
-            row.Cells.Add(CellHeader("Дата, время"));
-            row.Cells.Add(CellHeader(@"Уровень сахара крови (ммоль\л)"));
-            TableRowGroup RowGroupHeader = new();
-            RowGroupHeader.Rows.Add(row);
-            table.RowGroups.Add(RowGroupHeader);
-
-            row = new();
-            row.Cells.Add(CellContent(person.Sugars, "left"));
-            row.Cells.Add(CellContent(person.Sugars, "riht"));
-            TableRowGroup RowGroupContent = new();
-            RowGroupContent.Rows.Add(row);
-            table.RowGroups.Add(RowGroupContent);
-
-            Blocks.Add(table);
-
-            
-        }
-
-        private TableCell CellHeader(string text)
-        {
-            TableCell tableCell = new()
-            {
-                Background = Brushes.White,
-                TextAlignment = TextAlignment.Center,
-                Padding = new Thickness(0, 0, 0, 15)
-            };
-
-            //tableCell.Blocks.Add(new LineBreak());
-
-            Paragraph paragraph = new();
-
-            paragraph.Inlines.Add(new LineBreak());
-            //paragraph = new(new Run(text));
-            paragraph.Inlines.Add(new Run(text));
-            //paragraph.Inlines.Add(new Run("\n"));
-            //paragraph.MinWidowLines = 30;
-            //Padding = new Thickness(0,10,0,10)
-
-            tableCell.Blocks.Add(paragraph);
-            return tableCell;
-        }
-
-        private TableCell CellContent(List<Sugar> sugars, string column)
-        {
-            TableCell tableCell = new()
-            {
-                Background = Brushes.White,
-                TextAlignment = TextAlignment.Center
-            };
-
-            Paragraph paragraph = new();
-
-            paragraph.Inlines.Add(new LineBreak());
-
-            //tableCell.Blocks.Add(paragraph);
-            //tableCell.Blocks.Add(new Paragraph());
-            if (column == "left")
-            {
-                foreach (var item in sugars)
-                {
-                    //Paragraph paragraph = new();
-                    //paragraph.Inlines.Add(new LineBreak());
-                    paragraph.Inlines.Add(new Run($"{item.DateOnly} {item.TimeOnly}\r\n"));
-                    paragraph.LineHeight = 30;
-                    tableCell.Blocks.Add(paragraph);
-                    //tableCell.Blocks.Add(new Paragraph(new Run($"{item.DateOnly} {item.TimeOnly}")));
-                }                
-            }
-            else
-            {
-                foreach (var item in sugars)
-                {
-                    //Paragraph paragraph = new();
-                    //paragraph.Inlines.Add(new LineBreak());
-                    paragraph.Inlines.Add(new Run($"{item.Level}\r\n"));
-                    tableCell.Blocks.Add(paragraph);
-                    //tableCell.Blocks.Add(new Paragraph(new Run(item.Level)));
-                }
-            }
-
-            tableCell.Blocks.Add(paragraph);
-
-            tableCell.Blocks.Add(new Paragraph());
-            tableCell.Blocks.Add(new Paragraph());
-
-            return tableCell;
-        }
-        
-        private static Paragraph TextParagraphCenter(string text)
-        {
-            return new Paragraph(new Run(text))
-            {
-                TextAlignment = TextAlignment.Center
-            };
-        }             
+        };
     }
 }
 
